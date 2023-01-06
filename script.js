a2443e8b5618eb0c3dccb762f81a38c23d570ea0
Tim-KILLER <abdulmanovtumur@mail.ru>
2023-01-06 19:53:42 +0300

    answer

--- script.js
+++ script.js
@@ -15,3 +15,22 @@ const divis = (a,b) =>{
     return result
 }
 console.log(divis(10,10))
+
+/*
+true + false = 1
+8 / "2" = 4
+"number" + 5 + 1 = "number51"
+5 + 1 + "number" = "6number"
+7 && 2 = 2
+2 && 7 = 7
+null + 1 = 1
+undefined + 1 = nan
+"five" + +"2" = five2
+'true' == true = false
+false == 'false' = false
+null == '' = true
+!!"false" == !!"true" = true
+"4" - 3 = 1
+"4px" - 3 = nan
+0 || "0" && 1  = 1
+*/
